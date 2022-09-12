c47f6c02f0ec9c0c76f9def29ebd3358b7dcda7d
ibtesam shakil ansari <ibtesamshakil@gmail.com>
2022-09-12 22:05:25 +0500

    added navigation on button click

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+    const router = useRouter()
+
+    const clickHandler = () => {
+        alert("navigating to product")
+        router.push("/product")
+        // router.replace("/product")
+    }
     return (
         <>
-        <h1>Home Pages</h1>
-        <p>
-            <Link href="/blog">
-            <a>Blog page</a>
-            </Link>
-        </p>
-        <p>
-            <Link href="/product">
-            <a>Product page</a>
-            </Link>
-        </p>
+            <h1>Home Pages</h1>
+            <p>
+                <Link href="/blog">
+                <a>Blog page</a>
+                </Link>
+            </p>
+            <p>
+                <Link href="/product">
+                <a>Product page</a>
+                </Link>
+            </p>
+            <button onClick={clickHandler}>
+                CLICK ME
+            </button>
         </>
     )
 }

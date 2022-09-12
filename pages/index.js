04bc7a82c8c804ff8ca9ceb155a0c37b79549eea
ibtesam shakil ansari <ibtesamshakil@gmail.com>
2022-09-12 21:56:19 +0500

    implented linked based routing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import Link from "next/link";
+
 const Home = () => {
     return (
+        <>
         <h1>Home Pages</h1>
+        <p>
+            <Link href="/blog">
+            <a>Blog page</a>
+            </Link>
+        </p>
+        <p>
+            <Link href="/product">
+            <a>Product page</a>
+            </Link>
+        </p>
+        </>
     )
 }
 
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,31 @@
-const Product = () => {
+import Link from "next/link";
+
+const Product = ({productId = 50}) => {
     return (
         <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <h2>
+                <Link href="/product/1" replace>
+                    <a>Product 1</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href="/product/2">
+                    <a>Product 2</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href="/product/3">
+                    <a>Product 3</a>
+                </Link>
+            </h2>
+            <h2>
+                <Link href={`product/${productId}`}>
+                    <a>Product {productId}</a>
+                </Link>
+            </h2>
         </>
     )
 }
